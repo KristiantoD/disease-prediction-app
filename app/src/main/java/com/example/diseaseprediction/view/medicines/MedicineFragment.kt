b84a99c3b154942360988d416a38f74c7ffc91d0
KristiantoD <kristiantodesun99@gmail.com>
2022-05-24 16:49:11 +0700

    create appbar

--- app/src/main/java/com/example/diseaseprediction/view/medicines/MedicineFragment.kt
+++ app/src/main/java/com/example/diseaseprediction/view/medicines/MedicineFragment.kt
@@ -2,31 +2,38 @@ package com.example.diseaseprediction.view.medicines
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.view.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.example.diseaseprediction.R
+import com.example.diseaseprediction.databinding.FragmentConsultationBinding
+import com.example.diseaseprediction.databinding.FragmentMedicineBinding
+import com.example.diseaseprediction.view.consultate.ConsultationViewModel
 
 class MedicineFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = MedicineFragment()
-    }
-
-    private lateinit var viewModel: MedicineViewModel
+    private var _binding: FragmentMedicineBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var viewModel : MedicineViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_medicine, container, false)
+    ): View {
+        viewModel = ViewModelProvider(this)[MedicineViewModel::class.java]
+        _binding = FragmentMedicineBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val toolbar: Toolbar = binding.toolbar
+
+        toolbar.inflateMenu(R.menu.settings_menu)
+        return root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MedicineViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
+//    override fun onActivityCreated(savedInstanceState: Bundle?) {
+//        super.onActivityCreated(savedInstanceState)
+//        // TODO: Use the ViewModel
+//    }
+
 
 }
