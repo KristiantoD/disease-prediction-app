8465d36e55adc8a201fb8005ba8373fe930a5888
KristiantoD <kristiantodesun99@gmail.com>
2022-06-09 22:39:23 +0700

    change info page ui

--- app/src/main/java/com/example/diseaseprediction/view/information/InfoFragment.kt
+++ app/src/main/java/com/example/diseaseprediction/view/information/InfoFragment.kt
@@ -3,18 +3,23 @@ package com.example.diseaseprediction.view.information
 import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.widget.Toolbar
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.preferencesDataStore
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.diseaseprediction.R
 import com.example.diseaseprediction.databinding.FragmentInfoBinding
 import com.example.diseaseprediction.model.Preference
 import com.example.diseaseprediction.view.ViewModelFactory
+import com.example.diseaseprediction.view.welcome.MainActivity
 
 private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
 
@@ -33,6 +38,32 @@ class InfoFragment : Fragment() {
         setupViewModel()
         setupAction()
         playAnimation()
+
+        val toolbar: Toolbar = binding.toolbar
+        toolbar.inflateMenu(R.menu.logout_menu)
+
+        toolbar.setOnMenuItemClickListener {
+            if (it.itemId == R.id.menu1) {
+                context?.let { fragmentContext ->
+                    AlertDialog.Builder(fragmentContext).apply {
+                        setTitle(getString(R.string.logout))
+                        setMessage(getString(R.string.logout_message))
+                        setPositiveButton(getString(R.string.logout)) { _, _ ->
+                            viewModel.logout()
+                            val intent = Intent(activity, MainActivity::class.java)
+                            activity?.startActivity(intent)
+                            activity?.finish()
+                        }
+                        setNegativeButton(getString(R.string.cancel)) { dialog, _ ->
+                            dialog.dismiss()
+                        }
+                        create()
+                        show()
+                    }
+                }
+            }
+            true
+        }
 
         return root
     }
@@ -62,7 +93,6 @@ class InfoFragment : Fragment() {
     }
 
     private fun playAnimation() {
-        val mainCard = ObjectAnimator.ofFloat(binding.cardMain, View.ALPHA, 1f).setDuration(300)
         val profileCard =
             ObjectAnimator.ofFloat(binding.cardProfile, View.ALPHA, 1f).setDuration(300)
         val aboutCard = ObjectAnimator.ofFloat(binding.cardAbout, View.ALPHA, 1f).setDuration(300)
@@ -91,7 +121,7 @@ class InfoFragment : Fragment() {
         }
 
         AnimatorSet().apply {
-            playSequentially(mainCard, together1, together2, together3, profileName, profileEmail)
+            playSequentially(together1, together2, together3, profileName, profileEmail)
             start()
         }
     }
--- app/src/main/java/com/example/diseaseprediction/view/information/InfoViewModel.kt
+++ app/src/main/java/com/example/diseaseprediction/view/information/InfoViewModel.kt
@@ -3,11 +3,20 @@ package com.example.diseaseprediction.view.information
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import com.example.diseaseprediction.model.AuthorizationModel
 import com.example.diseaseprediction.model.Preference
+import kotlinx.coroutines.launch
 
 class InfoViewModel(private val pref: Preference) : ViewModel() {
     fun authorize(): LiveData<AuthorizationModel> {
         return pref.authorize().asLiveData()
     }
+
+    fun logout() {
+        viewModelScope.launch {
+            pref.logout()
+        }
+    }
+
 }
